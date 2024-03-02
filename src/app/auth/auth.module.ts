382723360f3e3510d85f311e8ae5f091cbf1131d
Aman07a <aman16.aa17@gmail.com>
2024-03-02 11:29:27 +0100

    Adding a Signup Form

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
@@ -9,6 +10,6 @@ import { SignoutComponent } from './signout/signout.component';
 
 @NgModule({
   declarations: [SigninComponent, SignupComponent, SignoutComponent],
-  imports: [CommonModule, AuthRoutingModule, SharedModule],
+  imports: [CommonModule, AuthRoutingModule, SharedModule, ReactiveFormsModule],
 })
 export class AuthModule {}
