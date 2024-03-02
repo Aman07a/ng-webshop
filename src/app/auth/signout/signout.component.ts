89a1f83109fc4621c2b592a571ef3cb83200a071
Aman07a <aman16.aa17@gmail.com>
2024-03-02 11:24:54 +0100

    Auth Generation

---
+++ src/app/auth/signout/signout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signout',
+  templateUrl: './signout.component.html',
+  styleUrls: ['./signout.component.css'],
+})
+export class SignoutComponent {}
