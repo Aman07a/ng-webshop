47af461cf1b5dc5ec7017c25d4eeaa4716a4301f
Aman07a <aman16.aa17@gmail.com>
2024-03-06 14:39:46 +0100

    Automated Signouts

--- src/app/auth/signout/signout.component.ts
+++ src/app/auth/signout/signout.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-signout',
   templateUrl: './signout.component.html',
   styleUrls: ['./signout.component.css'],
 })
-export class SignoutComponent {}
+export class SignoutComponent implements OnInit {
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.signout().subscribe(() => {
+      // Navigate the user back to a signin page
+    });
+  }
+}
