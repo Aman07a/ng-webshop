737391bca206a580d6cb20d0bab9b5a5e9cc1809
Aman07a <aman16.aa17@gmail.com>
2024-03-06 16:06:05 +0100

    Restricting Routing with Guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { authGuard } from './auth.guard';
+
 const routes: Routes = [
   {
     path: 'products',
+    canActivate: [authGuard],
     loadChildren: () =>
       import('./products/products.module').then((mod) => mod.ProductsModule),
   },
