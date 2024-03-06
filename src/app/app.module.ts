daccbd2ebb1cfdb5667a724aa0eb06a408366bde
Aman07a <aman16.aa17@gmail.com>
2024-03-06 15:24:09 +0100

    Products Module Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
 import { AuthHttpInterceptor } from './auth/auth-http.interceptor';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
