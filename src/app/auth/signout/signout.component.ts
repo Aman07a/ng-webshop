8e23f32489086c9528e74893f5cee8e637436032
Aman07a <aman16.aa17@gmail.com>
2024-03-06 14:40:33 +0100

    Programmatic Navigation

--- src/app/auth/signout/signout.component.ts
+++ src/app/auth/signout/signout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { AuthService } from 'src/app/auth.service';
 
 @Component({
@@ -7,11 +9,11 @@ import { AuthService } from 'src/app/auth.service';
   styleUrls: ['./signout.component.css'],
 })
 export class SignoutComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
     this.authService.signout().subscribe(() => {
-      // Navigate the user back to a signin page
+      this.router.navigateByUrl('/');
     });
   }
 }
