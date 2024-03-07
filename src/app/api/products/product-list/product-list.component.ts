091bb84b91bd0cb626f6673c8fabc2df66fee943
Aman07a <aman16.aa17@gmail.com>
2024-03-07 14:24:49 +0100

    Added Cards to Product List

--- src/app/api/products/product-list/product-list.component.ts
+++ src/app/api/products/product-list/product-list.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Product {
+  id: string;
+  name: string;
+  category: string;
+  description: string;
+  images: {
+    FV: string;
+    BV: string;
+    SV: string;
+    POV: string;
+  };
+  price: string;
+}
 
 @Component({
   selector: 'app-product-list',
@@ -6,8 +21,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-  constructor() {}
+  products: Product[] = [];
 
-  ngOnInit() {}
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchProducts();
+  }
+
+  fetchProducts() {
+    this.http.get<Product[]>('assets/api/products.json').subscribe(
+      (data) => {
+        this.products = data;
+      },
+      (error) => {
+        console.error('Error fetching products:', error);
+      }
+    );
+  }
+
+  addToCart(product: Product) {
+    console.log('Product added to cart:', product);
+  }
 }
-
